2b2705d6c28c5b63e020d956f46714857c7f612c
Jimmy Yazji <jimmyyazji98@gmail.com>
2021-06-18 15:12:27 +0300

    Added Placeholders

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,14 +18,14 @@
                     <div>
                         <x-label for="first_name" :value="__('First Name')" />
 
-                        <x-input id="first_name" class="block mt-1 w-full" type="text" name="first_name" :value="old('first_name')"
-                            required autofocus />
+                        <x-input id="first_name" class="block mt-1 w-full" type="text" name="first_name"
+                            :value="old('first_name')" placeholder="First Name" required autofocus />
                     </div>
                     <div>
                         <x-label for="last_name" :value="__('Last Name')" />
 
-                        <x-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')"
-                            required autofocus />
+                        <x-input id="last_name" class="block mt-1 w-full" type="text" name="last_name"
+                            :value="old('last_name')" placeholder="Last Name" required autofocus />
                     </div>
                 </div>
             </div>
@@ -35,24 +35,26 @@
                 <x-label for="email" :value="__('Email')" />
 
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
-                    required />
+                    placeholder="Email" required />
             </div>
             <div class="grid grid-row-2 gap-6 mt-2">
                 <div class="grid grid-cols-2 gap-6">
                     <div>
                         <x-label for="stdsn" :value="__('Serial Number')" />
 
-                        <x-input id="serial_number" class="block mt-1 w-full" type="text" name="serial_number" :value="old('serial_number')"
-                            required autofocus />
+                        <x-input id="serial_number" class="block mt-1 w-full" type="text" name="serial_number"
+                            :value="old('serial_number')" placeholder="Serial Number" autofocus />
                     </div>
                     <div>
                         <x-label for="department" :value="__('Department')" />
 
-                        <select id="department" name="department" autocomplete="dept_name" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 sm:text-sm">
-                        <option value="">Select Department</option>
-                        @foreach ($depts as $dept)
-                        <option value="{{ $dept->id }}" {{ $dept->id == old('dept_id') ? 'selected' : '' }} }}>{{ $dept->name }}</option>
-                        @endforeach
+                        <select id="department" name="department" autocomplete="dept_name"
+                            class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 sm:text-sm">
+                            <option selected disabled value="">Select Department</option>
+                            @foreach ($depts as $dept)
+                            <option value="{{ $dept->id }}" {{ $dept->id == old('dept_id') ? 'selected' : '' }} }}>
+                                {{ $dept->name }}</option>
+                            @endforeach
                         </select>
                     </div>
                 </div>
@@ -63,7 +65,7 @@
                 <x-label for="password" :value="__('Password')" />
 
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                    autocomplete="new-password" />
+                    placeholder="Password" autocomplete="new-password" />
             </div>
 
             <!-- Confirm Password -->
@@ -71,7 +73,7 @@
                 <x-label for="password_confirmation" :value="__('Confirm Password')" />
 
                 <x-input id="password_confirmation" class="block mt-1 w-full" type="password"
-                    name="password_confirmation" required />
+                    placeholder="Confirm Password" name="password_confirmation" required />
             </div>
 
             <div class="flex items-center justify-end mt-4">
