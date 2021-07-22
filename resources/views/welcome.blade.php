f423ab7c1a73bbc001d89b9b89d9011b8394b825
Jimmy Yazji <jimmyyazji98@gmail.com>
2021-07-22 20:34:50 +0300

    fixed style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
                 </svg>
                 <div>
                     <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
-                        <nav x-data="{ open: false } class="relative flex items-center justify-between sm:h-10 lg:justify-start"
+                        <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start"
                             aria-label="Global">
                             <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
                                 <div class="flex items-center justify-between w-full md:w-auto">
@@ -48,37 +48,34 @@
                                 </div>
                             </div>
                             <div class="hidden md:block md:ml-10 md:pr-4">
-                                <a href="#"
-                                    class="font-bold text-gray-500 hover:text-gray-900 px-3 py-2 rounded-full hover:bg-gray-100">Product</a>
+                                <a href="#" class="font-bold text-gray-500 hover:text-gray-900 px-3 py-2 rounded-full hover:bg-gray-100">Product</a>
 
-                                <a href="#"
-                                    class="font-bold text-gray-500 hover:text-gray-900 px-3 py-2 rounded-full hover:bg-gray-100">Features</a>
+                                <a href="#" class="font-bold text-gray-500 hover:text-gray-900 px-3 py-2 rounded-full hover:bg-gray-100">Features</a>
 
-                                <a href="#"
-                                    class="font-bold text-gray-500 hover:text-gray-900 px-3 py-2 rounded-full hover:bg-gray-100">Marketplace</a>
+                                <a href="#" class="font-bold text-gray-500 hover:text-gray-900 px-3 py-2 rounded-full hover:bg-gray-100">Marketplace</a>
 
-                                <a href="#"
-                                    class="font-bold text-gray-500 hover:text-gray-900 px-3 py-2 rounded-full hover:bg-gray-100">Company</a>
+                                <a href="#" class="font-bold text-gray-500 hover:text-gray-900 px-3 py-2 rounded-full hover:bg-gray-100">Company</a>
 
                                 <a href="{{ route('login') }}"
-                                    class="font-bold text-indigo-600 px-3 py-2 rounded-full hover:bg-gray-100 hover:text-indigo-500">Log
-                                    in</a>
+                                    class="font-bold text-indigo-600 px-3 py-2 rounded-full hover:bg-gray-100 hover:text-indigo-500">Log in</a>
                             </div>
                         </nav>
                     </div>
                     <div class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
                         <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
                             <div class="px-5 pt-4 flex items-center justify-between">
-                                <x-application-logo class="block h-14 w-auto fill-current text-gray-500" />
+                                <div>
+                                    <img class="h-8 w-auto"
+                                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="">
+                                </div>
                                 <div class="-mr-2">
-                                    <button @click="open = ! open"
-                                        class="inline-flex items-center justify-center p-2 rounded-md text-gray-600 hover:bg-gray-200 focus:outline-none transition duration-150 ease-in-out">
-                                        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                            <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
-                                                stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="M4 6h16M4 12h16M4 18h16" />
-                                            <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden"
-                                                stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    <button type="button"
+                                        class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                        <span class="sr-only">Close main menu</span>
+                                        <!-- Heroicon name: outline/x -->
+                                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                            viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                 d="M6 18L18 6M6 6l12 12" />
                                         </svg>
                                     </button>
@@ -156,6 +153,6 @@
     <script src="{{ asset('js/app.js') }}"></script>
 
 </body>
-<x-footer />
+<x-footer/>
 
 </html>
